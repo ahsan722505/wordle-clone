69088ce99f29a22c23cb1c4c471c4e48a834913f
ahsan722505 <ahsantahir722505@gmail.com>
2022-02-05 02:19:12 +0500

    jdfn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
 import Grid from './components/Grid';
 import Keyboard from './components/Keyboard';
+import { getWord } from './helpers/util';
+import { useState,useEffect,useCallback } from 'react';
 function App() {
+  const [currentRow,setCurrentRow]=useState(0);
+  const [letters,setLetters]=useState([]);
+  const [word,setWord]=useState(getWord());
+  useEffect(()=>{
+    alert(word);
+  },[]);
+  
+  const handleKeyDown=useCallback((e)=>{
+    if(((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) && (letters.length<=4)){
+      const letter=e.key.toUpperCase();
+      setLetters(state=>{
+        let newState=[...state];
+        newState.push(letter);
+        return newState;
+      })
+    };
+  },[]);
+  useEffect(()=>{
+    window.addEventListener("keydown",handleKeyDown);
+    return ()=>{
+      window.removeEventListener("keydown",handleKeyDown);
+    }
+  },[handleKeyDown]);
+  
+
   return (
     <div className="App">
-      <Grid/>
+      <Grid letters={letters} currentRow={currentRow}/>
       <Keyboard/>
     </div>
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,16 +1,17 @@
 import styles from "./Grid.module.css";
 import { useState } from "react";
 import { createGrid } from "../helpers/util";
-const Grid=()=>{
+const Grid=({letters,currentRow})=>{
     const [grid,setGrid]=useState(createGrid(6,5));
+    let colCount=0;
     return(
         <div className={styles.grid}>
-            {grid.map(eachRow=>{
+            {grid.map((eachRow,rowCount)=>{
                 return(
                     <div className={styles.row}>
                             {eachRow.map(eachCell=>{
                                 return(
-                                    <div className={styles.cell}></div>
+                                    <div className={styles.cell}><span>{currentRow === rowCount && letters[colCount++]}</span></div>
                                 )
                             })}
                     </div>
